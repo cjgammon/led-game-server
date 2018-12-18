4ffaa5043186e193552b3cbcc05a1edf775d6a6a
cjgammon <gammon@adobe.com>
2018-12-18 11:17:07 -0600

    fix port

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var socketIO = require('socket.io');
+const PORT = process.env.PORT || 5000
 
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 
-app.set('port', 80);
+app.set('port', PORT);
 app.use('/static', express.static(__dirname + '/static'));
 
 // Routing
@@ -16,7 +17,7 @@ app.get('/', function(request, response) {
 });
 
 // Starts the server.
-server.listen(80, function() {
+server.listen(PORT, function() {
   console.log('Starting server on port 5000');
 });
 
